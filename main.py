c749da97d5a206d011541e8128c4500ed3c27cdd
chongjinisnotavailableisavailable <c.j.chong.jin@gmail.com>
2023-12-11 23:48:10 +0800

    modular house cleaning; script can now open a docx

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from folder_creater import create_folder
 from variable_test import folder_name
 from doc_opener import open_new_doc
+from what_is_your_format import format_check
 
 
 def main():
     create_folder() #ensures folder is there
     open_new_doc(folder_name) #creates a document in folder
+    format_check() #looks at what format of the report (i.e. generic, detailed, etc) is required and imports the relevant modules
     
 """
-    open_new_doc()
-    format_checker()
-    format_select()
+    format_required_checker & import()
     report_writer()
     save_and_close()
 
---
+++ what_is_your_format/__init__.py
@@ -0,0 +1,4 @@
+from format_nandesuka import format_check
+from variable_test import reports_format
+
+print(f"Selected format is {reports_format}")
